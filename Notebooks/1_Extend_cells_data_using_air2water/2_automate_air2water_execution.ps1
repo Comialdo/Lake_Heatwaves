45d8fcc5d6242b343cfbb8731ddd97cc63086520
Comialdo <146339337+Comialdo@users.noreply.github.com>
2024-10-12 15:31:08 +0200

    Add files via upload

---
+++ Notebooks/1_Extend_cells_data_using_air2water/2_automate_air2water_execution.ps1
@@ -0,0 +1,50 @@
+﻿# Change to the directory where the executable and other files are located
+Set-Location "..\air2water-master"
+
+# Define the paths to the files (now relative to the new location)
+$coordinatesFile = "Garda\coordinates.txt"
+$inputFile = "input.txt"
+$executable = "air2water_v2.0.exe"
+
+# Read the coordinates from the file
+$coordinates = Get-Content $coordinatesFile
+
+# Loop through each line in the coordinates file
+foreach ($coordinate in $coordinates) {
+    $parts = $coordinate -split '_'
+    $lat = $parts[0]
+    $lon = $parts[1]
+
+    # Create the new content for the input file
+    $inputContent = @"
+! Input file
+Garda                ! name of the lake, river, ... 
+$lat                 ! name/ID of the air station 
+$lon                 ! name/ID of the water station 
+c                    ! type of series: c=continuous, m=mean year 
+1d                   ! time resolution: 1d=daily, nw=n weeks (n=1,2,...), 1m=monthly 
+4                    ! version: 1=a2w 4 par; 2=a2w 6 par, 3=a2w 8 par 
+0                    ! Threshold temperature for ice formation 
+RMS                  ! objective function: KGE, NSE, RMS 
+CRN                  ! numerical model: CRN, RK2, RK4 or EUL  
+PSO                  ! PSO, FORWARD, LATHYP 
+0.01                 ! minimum percentage of data: 0...1  
+500                  ! nrun 
+999                  ! mineff_index  
+1                    ! log_flag  
+0.9                  ! Courant number 
+"@
+
+    # Write the new content to the input file
+    Set-Content -Path $inputFile -Value $inputContent
+
+    # Add a delay before running the executable
+    #Start-Sleep -Seconds 1
+
+    # Run the executable and wait for it to finish
+    Start-Process -FilePath $executable -Wait
+
+    Write-Output "Processed row with lat: $lat and lon: $lon"
+}
+
+Write-Output "All rows processed."
---
+++ README.md
@@ -0,0 +1,13 @@
+# Lake_Heatwaves
+
+## How to use
+
+### Setup python environment
+Build a conda environment from the provided YAML file in `setup/satellite.yaml`:<br/>
+`conda env create --name satellite --file setup/satellite.yaml`
+
+### Download Air2Water model (Piccolroaz et al. 2013) 
+Download in `Notebooks/air2water-master` the model from this link: https://github.com/marcotoffolon/air2water
+
+### Download marineHeatWaves.py (Hobday et al. 2016)
+Download in `Notebooks` the marineHeatWaves.py script from this link: https://github.com/ecjoliver/marineHeatWaves
